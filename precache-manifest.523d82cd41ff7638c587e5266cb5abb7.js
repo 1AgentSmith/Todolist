103b7e082f6cf7fd4858b2fc56429c331f4e27d4
Valerii22 <valera.vakalov22@gmail.com>
2021-11-30 12:27:48 +0100

    Updates

---
+++ precache-manifest.523d82cd41ff7638c587e5266cb5abb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f444f5b25627a5280344a306b3d25e2",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "4f857bd226dfc6242a19",
+    "url": "/Todolist/static/css/main.25c55ade.chunk.css"
+  },
+  {
+    "revision": "53d6cd089744677fc35b",
+    "url": "/Todolist/static/js/2.ad4ad9ed.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist/static/js/2.ad4ad9ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f857bd226dfc6242a19",
+    "url": "/Todolist/static/js/main.6ffafc3b.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
